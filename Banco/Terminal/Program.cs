68951f19287fac8363aa81a3a02565f714d3dd80
LUCAS GOMES REDIGOLO <lucas_redigolo@hotmail.com>
2017-11-01 20:31:09 -0200

    Fim aula Enapsulmento

---
+++ Banco/Terminal/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using dados;
+
+namespace Terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta = new Conta();
+            conta.Depositar(250);
+
+            System.Console.WriteLine(conta.MeuSaldo());
+        }
+    }
+}
---
+++ Banco/dados/Conta.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+
+namespace dados
+{
+    /// <summary>
+    /// Procedimentos na conta do usuário
+    /// </summary>
+    public class Conta
+    {
+        public string Banco { get; set; }
+        public string Agencia { get; set; }
+        public string ContaCorrente { get; set; }
+        public double Saldo { get; private set; }
+
+        /// <summary>
+        /// Retira uma quantidade da conta 
+        /// </summary>
+        /// <param name="valor"></param>
+        public void Sacar(double valor)
+        {
+            this.Saldo -= valor;
+        }
+        /// <summary>
+        /// Realiza um depósito na conta do cliente 
+        /// </summary>
+        /// <param name="valor"></param>
+        public void Depositar(double valor)
+        {
+            this.Saldo += valor;
+        }
+        /// <summary>
+        /// Retorna o saldo do cliente 
+        /// </summary>
+        /// <returns></returns>
+        public double MeuSaldo()
+        {
+            return this.Saldo;
+        }
+    }
+}
